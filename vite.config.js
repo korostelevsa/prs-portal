599eb6cef61c0f9be4f2b5c9242e55fbfa953cdc
korostelevsa <korostelevsa@genomed.ru>
2025-08-13 17:42:54 +0300

    chore(pages): set Vite base to /prs-portal/ and publish dist to /docs

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
-import { defineConfig } from 'vite'
+﻿import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/prs-portal/',
   plugins: [react()],
 })
+
